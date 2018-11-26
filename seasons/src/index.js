545926f839cfe5bbed178a2aed88d2c8fe61371f
jbillena-mmp <joseph@multimediaplus.com>
2018-11-26 13:45:09 -0500

    Section 6 - Seasons project

---
+++ seasons/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it rerendered!');
+    }
+
+    renderContent() {
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (this.state.lat && !this.state.errorMessage) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return <div className="border red">{this.renderContent()}</div>
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
